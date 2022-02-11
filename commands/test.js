6cb9ed9af72036cde4927fa4d55adbcd205c5f13
Max <massimobajireanu6@gmail.com>
2022-02-11 22:46:16 +0100

    AggiuntiExport

---
+++ commands/test.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "test",
+    description: "Comando di test",
+    execute(message, args) {
+        message.channel.send("TEST");
+    }
