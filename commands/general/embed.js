1aafe6f9491597880c37003487c4b387717e31db
Max <massimobajireanu6@gmail.com>
2022-02-11 23:20:24 +0100

    .

--- commands/general/embed.js
+++ commands/general/embed.js
@@ -5,7 +5,13 @@ module.exports = {
     description: "Comando embed",
     execute(message, args) {
         const embed = new Discord.MessageEmbed()
-            .setTitle("Ciao")
+            .setTitle("Titolo") //Titolo
+            .setColor("#34a42d") // Colore principale
+            .setAuthor("Autore")
+            .setDescription("Descrizione") //Descrizione
+            //Aggiungere elementi
+            .addField("Titolo", "Contenuto", true) //QUI TUTTI I PARAMETRI SONO OBBLIGATORI - True o false = se questo elemento deve essere in linea con gli altri
+            .setImage("https://cdn.discordapp.com/attachments/857322373686886420/860472233792765982/realm.gif") //Immagine  
         message.channel.send({embeds: [embed]})
         
     }
