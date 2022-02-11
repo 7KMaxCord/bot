29e2b6be49eec3c70c7927e2061b474c6326441d
Max <massimobajireanu6@gmail.com>
2022-02-11 22:13:01 +0100

    RimossoTokenSecret

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client(
 	{ intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
 )
 
-client.login(process.env.token)
+client.login("OTM4Nzg2NTYzODAwODM0MDU4.YfvXHg.f1jgL6Qs4nKn6WrMH5cDsKTxr-o")
 
 client.on("ready", () => {
 	console.log("Bot startato con successo!")
