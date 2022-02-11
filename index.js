ba28cae1bcd7fa3703a1ed9090b9c7f57e80f193
Max <massimobajireanu6@gmail.com>
2022-02-11 22:54:31 +0100

    fix2

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require("discord.js")
+const client = new Discord.Client(
+	{ intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
+)
 
-client.login(process.env.token);
+client.login(process.env.token)
+
+client.on("ready", () => {
+	console.log("Bot startato con successo!")
+})
 
 const fs = require("fs");
 
@@ -13,39 +19,15 @@ for (const file of commandsFiles) {
     client.commands.set(command.name, command);
 }
 
-const commandsFolder = fs.readdirSync("./commands");
-for (const folder of commandsFolder) {
-    const commandsFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith(".js"));
-    for (const file of commandsFiles) {
-        const command = require(`./commands/${folder}/${file}`);
-        client.commands.set(command.name, command);
-    }
-}
-
-const eventsFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
-for (const file of eventsFiles) {
-    const event = require(`./events/${file}`);
-    client.on(event.name, (...args) => event.execute(...args))
-}
-
 client.on("message", message => {
-    const prefix = "!";
+	const prefix = "!";
+	if (!message.content.startWhith(prefix) || message.author.bot) return
 
-    if (!message.content.startsWith(prefix) || message.author.bot) return
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
+	const command = args.shift().toLowerCase();
 
-    if (!client.commands.has(command) && !client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command))) return
+	if(!client.commands.has(command)) return
 
-    var comando = client.commands.get(command) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command))
-
-    if (comando.onlyStaff) {
-        if (!message.member.hasPermission("ADMINISTRATOR")) {
-            message.channel.send("Non hai il permesso di eseguire questo comando")
-            return
-        }
-    }
-
-    comando.execute(message, args);
+	client.commands.get(command).execute(message);
 })
