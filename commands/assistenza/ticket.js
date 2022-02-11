b995261ddbdc8adb609db23df2cef328a5e23d6e
Max <massimobajireanu6@gmail.com>
2022-02-11 23:34:23 +0100

    .

--- commands/assistenza/ticket.js
+++ commands/assistenza/ticket.js
@@ -1,4 +1,7 @@
 const Discord = require('discord.js');
+const client = new Discord.Client(
+    { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
+)
 
 //Prima di tutto mandare il messaggio del ticket
 client.on("messageCreate", message => {
